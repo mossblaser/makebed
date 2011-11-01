db17d1736ca304372abc00141bafcc15c091fd7c
Jonathan Heathcote <mail@jhnet.co.uk>
2011-11-01 13:30:19 +0000

    Added initial libc-like function library

---
+++ cutil/cutil.c
@@ -0,0 +1,8 @@
+/**
+ * A subset of libc which has been implemented "as needed" for the mbed-freertos
+ * distribution by Jonathan Heathcote. It may be buggy, subtly ignore the spec
+ * and it obviously miles from being incomplete.
+ */
+
+
+
