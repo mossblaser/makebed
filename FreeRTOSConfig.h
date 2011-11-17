48e10400e442df9ab1cb203667115e3ee7e0c41d
Jonathan Heathcote <mail@jhnet.co.uk>
2011-11-17 00:40:25 +0000

    Fixed clock speed definition in FreeRTOS Config

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -70,7 +70,7 @@
 #define configUSE_IDLE_HOOK			0
 #define configMAX_PRIORITIES		( ( unsigned portBASE_TYPE ) 5 )
 #define configUSE_TICK_HOOK			1
-#define configCPU_CLOCK_HZ			( ( unsigned long ) 99000000 )
+#define configCPU_CLOCK_HZ			( ( unsigned long ) 100000000 )
 #define configTICK_RATE_HZ			( ( portTickType ) 1000 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 80 )
 #define configTOTAL_HEAP_SIZE		( ( size_t ) ( 19 * 1024 ) )
